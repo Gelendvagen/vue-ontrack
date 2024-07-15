661d1c67c5ad3433d6349eda1719e3186bd8c94f
Angelina_Prokina <angelinaprokina@yandex.ru>
2024-07-16 00:34:23 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,10 @@
 <script setup>
-  import {CheckCircleIcon} from '@heroicons/vue/24/solid'
-  import {ClockIcon, ListBulletIcon, ChartBarIcon} from '@heroicons/vue/24/outline'
-
-  const navItems = ['timeline', 'activities', 'progress']
+  import TheHeader from './components/TheHeader.vue'
+  import TheNav from './components/TheNav.vue'
 </script>
 
 <template>
-  <header class="sticky top-0 z-20 flex item-center justify-between border-b bg-white p-3">
-    <a href="#">
-      <img src="./assets/logo.svg" alt="Logo" class="h-9">
-    </a>
-    <a href="#" class="text-sm">
-      <div v-if="true" class="flex items-center gap-1">
-        Day complete
-        <CheckCircleIcon class="h-7 text-green-500" />
-      </div>
-
-      <div v-else class="flex items-center gap-1">
-        <div>Progress: <span class="font-mono">20%</span></div>
-        <div class="h-3 w-3 rounded-full bg-red-500"></div>
-
-      </div>
-    </a>
-  </header>
-  <main class="flex flex-grow flex-col">
-    <nav class="sticky bottom-0 z-10 bg-white">
-      <ul class="flex items-center justify-around border-t">
-        <li v-for="page in navItems" :key="page" class="flex-1">
-          <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-            <ClockIcon class="h-6 w-6" /> {{ page }}
-          </a>
-        </li>
-        
-        
-        
-        
-        <!-- <li class="flex-1">
-          <a class="flex flex-col items-center p-2 text-xs capitalize" href="#activities">
-            <ListBulletIcon class="h-6 w-6" /> activities
-          </a>
-        </li>
-        <li class="flex-1">
-          <a class="flex flex-col items-center p-2 text-xs capitalize" href="#progress">
-            <ChartBarIcon class="h-6 w-6" /> progress
-          </a>
-        </li> -->
-      </ul>
-
-    </nav>
-
-  </main>
+  <TheHeader />
+  <main class="flex flex-grow flex-col"></main>
+  <TheNav />
 </template>
