96394b13006d7f7d43872e132bec0f283f210dd0
Angelina_Prokina <angelinaprokina@yandex.ru>
2024-07-17 02:48:59 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <script setup>
   import TheHeader from './components/TheHeader.vue'
   import TheNav from './components/TheNav.vue'
+  import TheTimeLine from './pages/TheTimeLine.vue'
+  import TheActivities from './pages/TheActivities.vue'
+  import TheProgress from './pages/TheProgress.vue'
+  import {PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS} from './constants.js'
+  import {ref} from 'vue'
+  import { normalizePageHash } from './functions'
+
+  const currentPage = ref(normalizePageHash())
+
+  function goTo(page) {
+    currentPage.value = page
+  }
 </script>
 
 <template>
-  <TheHeader />
-  <main class="flex flex-grow flex-col"></main>
-  <TheNav />
+  <TheHeader @go-to-time-line="goTo(PAGE_TIMELINE)" @go-to-progress="goTo(PAGE_PROGRESS)" />
+  <main class="flex flex-grow flex-col">
+    <TheTimeLine v-show="currentPage === PAGE_TIMELINE" />
+    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
+    <TheProgress v-show="currentPage === PAGE_PROGRESS" />
+  </main>
+  <TheNav :current-page="currentPage" @navigate="goTo($event)" />
 </template>
