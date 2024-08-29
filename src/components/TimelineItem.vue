094b4fcb8f1ae8488b1e1cf3df1e4c1774757c88
Angelina_Prokina <angelinaprokina@yandex.ru>
2024-08-29 23:59:04 +0300

    stopwatch marking

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -2,7 +2,9 @@
     import BaseSelect from './BaseSelect.vue'
     import {isActivityValid, isTimeLineItemValid, validateActivities, validateSelectOptions} from '../validators'
     import TimelineHour from './TimelineHour.vue'
-    import {NULLABLE_ACTIVITY} from '../constants'
+    import {BUTTON_TYPE_DANGER, BUTTON_TYPE_WARNING, BUTTON_TYPE_SUCCESS, NULLABLE_ACTIVITY} from '../constants'
+    import {ArrowPathIcon, PauseIcon, PlayIcon} from '@heroicons/vue/24/outline'
+    import BaseButton from './BaseButton.vue'
 
     const props = defineProps({
         timelineItem: {
@@ -44,5 +46,19 @@
             placeholder="Rest" 
             @select="selectActivity"
         />
+        <div class="flex w-full gap-2">
+            <BaseButton :type="BUTTON_TYPE_DANGER">
+                <ArrowPathIcon class="h-8" />
+            </BaseButton>
+            <div class="flex flex-grow items-center rounded bg-gray-100 px-2 font-mono text-3xl">
+                00:00:00
+            </div>
+            <BaseButton :type="BUTTON_TYPE_WARNING">
+                <PauseIcon class="h-8" />
+            </BaseButton>
+            <BaseButton :type="BUTTON_TYPE_SUCCESS">
+                <PlayIcon class="h-8" />
+            </BaseButton>
+        </div>
     </li>
 </template>
