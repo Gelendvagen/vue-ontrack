97c866588abc970bb4d1f2cb007696e41ac75e73
Angelina_Prokina <angelinaprokina@yandex.ru>
2024-09-03 20:26:28 +0300

    refactoring

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,10 +1,11 @@
 <script setup>
     import BaseButton from './BaseButton.vue'
-    import {XMarkIcon} from '@heroicons/vue/24/outline'
+    import {ICON_X_MARK} from '../icons'
     import {validateSelectOptions, isUndefinedOrNull, isSelectValueValid} from '../validators'
     import {computed} from 'vue'
     import {BUTTON_TYPE_NEUTRAL} from '../constants'
     import {normalizeSelectValue} from '../functions'
+    import BaseIcon from './BaseIcon.vue'
 
     const props = defineProps({
         selected: [String, Number],
@@ -33,16 +34,13 @@
 <template>
     <div class="flex gap-2">
         <BaseButton :type="BUTTON_TYPE_NEUTRAL" @click="select(null)">
-            <XMarkIcon class="h-8" />
+            <BaseIcon :name="ICON_X_MARK" />
         </BaseButton>
         <select class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl" @change="select($event.target.value)">
             <option :selected="isNotSelected" disabled value="">{{placeholder}}</option>
             <option v-for="{value, label} in options" :key="value" :value="value" selected="value === selected">
                 {{label}}
             </option>
-            <option value="">Coding</option>
-            <option value="">Reading</option>
-            <option value="">Training</option>
         </select>
     </div>
 </template>
